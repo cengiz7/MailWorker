b5a660c432728a29625bb98273c26076961537c2
Cengiz Üçgül <cengaver0708@gmail.com>
2018-07-09 15:38:28 +0300

    Update loggin_printing.go

--- loggin_printing.go
+++ loggin_printing.go
@@ -5,16 +5,25 @@ import (
 	"os"
 )
 
+// if you want  to append new logs to the existing log file, change the secon os.O_CREATE with os.RWONLY
 func CreateLogFile(){
-	f, err := os.OpenFile("logs.log", os.O_RDWR | os.O_CREATE | os.O_CREATE, 0666)
+	f, err := os.OpenFile(logPath, os.O_RDWR | os.O_CREATE | os.O_CREATE, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
 	log.SetOutput(f)
 }
 
-func FailOnError(err error, msg string) {
+
+// if you don't want to log succesfull processes, then leave the sccsmsg field while calling the FailOnError func.
+func LoggingChecking(err error, errmsg string,sccsmsg string) bool {
 	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
+		log.Fatalf("%s: %s", errmsg, err)
+		return true
+	} else {
+		if sccsmsg != ""{
+			log.Printf("%s",sccsmsg)
+		}
+		return false
 	}
 }
